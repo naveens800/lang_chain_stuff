1b726a96af1c3c72e3cdf5ea777fea7583bc6c12
Naveen Sargam <naveensargam123@gmail.com>
2024-04-28 13:56:06 +0530

    summary of a text using langchain and LLM model

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ from langchain.prompts import PromptTemplate
 
 llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
 
-translation_template = "Translate the following text from {source_language} to {target_language}: {text}"
-translation_prompt = PromptTemplate(input_variables=["source_language", "target_language", "text"], template=translation_template)
-translation_chain = LLMChain(llm=llm, prompt=translation_prompt)
+summarization_template = "Summarize the following text to one sentence: {text}"
+summarization_prompt = PromptTemplate(input_variables=["text"], template=summarization_template)
+summarization_chain = LLMChain(llm=llm, prompt=summarization_prompt)
 
-source_language = "English"
-target_language = "French"
-text = "Hi Naveen"
-translated_text = translation_chain.predict(source_language=source_language, target_language=target_language, text=text)
-print("Translated Text: ", translated_text)
+text = """LangChain provides many modules that can be used to build language model applications.
+Modules can be combined to create more complex applications, or be used individually for simple applications.
+The most basic building block of LangChain is calling an LLM on some input.
+Let’s walk through a simple example of how to do this. For this purpose,
+let’s pretend we are building a service that generates a company name based on what the company makes."""
+
+summarized_text = summarization_chain.predict(text=text)
+print("Translated Text: ", summarized_text)
